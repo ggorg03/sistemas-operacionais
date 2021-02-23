8f8d365e87abf67a9f5e98000d787a5c1b7e4b97
ggorg03 <ggorg03github.com>
2021-02-23 04:24:41 -0300

    Solução parcial da tarefa-1.2, ainda falta tratar algumas exceções e também tratar formato da saída para notação científica

---
+++ tarefa-1.2/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int f (int x){
+    return 5;
+}
+
+void* calcula_area_sobcurva (void* entrada){
+    // convertendo entrada para vetor de floats
+    float* entradas = (float*)entrada;
+    // coletando os parametros
+    float a = entradas[0];
+    float b = entradas[1];
+    int qtd_trpz = entradas[2];
+    // calculando a area dos trapezios
+    float h = (b - a) / qtd_trpz;
+    float area = (f(a) + f(b)) / 2;
+    for (int i = 1; i < qtd_trpz; i++){
+        area += f(a + i*h);
+    }
+    area *= h;
+    // retornando a area dos trapezios
+    pthread_exit((void*)(size_t)area);
+}
+
+int main(void){
+    float a;
+    float b;
+    int qtd_threads;
+    int qtd_trpz;
+    void* thread_return;
+    float area = 0;
+    // inicializando variaveis
+    a = 0;
+    b = 10;
+    qtd_trpz = 10;
+    qtd_threads = 5;
+    // calculando quantidade de trapezios por tread
+    int trpz_por_thread = (int) qtd_trpz / qtd_threads;
+    // incializando threads
+    pthread_t threads[qtd_threads];
+    // calculando o comprimeto de cada thread
+    float h = (b - a) / qtd_threads;
+    // rodando threads
+    for (int i = 0; i < qtd_threads; i++){
+        float entradas[] = {a + i*h, a + (i + 1)*h, trpz_por_thread};
+        pthread_create(&threads[i], NULL, calcula_area_sobcurva, (void*) entradas);
+    }
+    // coletando o resultado de cada thread e acumulando o valor na variavel 'area'
+    for (int i = 0; i < qtd_threads; i++) {
+        pthread_join(threads[i], &thread_return);
+        area += (int)(size_t)thread_return;
+    }
+    // printando a area sob a curva da funcao
+    printf("Area %f\n", area);
+    return 0;
+}
